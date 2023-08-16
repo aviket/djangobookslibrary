aca763a5f334fadcd00772408bcca732ec233599
abc <abc@abc.com>
2023-08-16 07:40:41 +0530

    manage bookshelf

--- booksApp (1)/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import book_list
-
-urlpatterns = [
-    path('', book_list, name='book_list'),
-]
--- booksApp (1)/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-from .models import Book
-
-def book_list(request):
-    books = Book.objects.all()
-    return render(request, 'booksApp/book_list.html', {'books': books})
-
